80b14886bd49e659c192ee4f584e72bfc0da24ee
AMINE <150825079+THEW00LY@users.noreply.github.com>
2026-03-27 09:47:15 +0000

    Refactor task scheduling with pthreads and barriers

--- pre_project.c
+++ pre_project.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <errno.h>
 #include <assert.h>
+#include <bits/pthreadtypes.h>
 
 #define load 10000
 
@@ -12,7 +13,7 @@
  long tv_nsec ;  Nanoseconds 
 }; */
 
-
+pthread_barrier_t barrier;
 
 void sleep_until_next_activation(struct timespec * next_activation) {
 	int err;
@@ -26,41 +27,77 @@ void sleep_until_next_activation(struct timespec * next_activation) {
 	} while (err !=0 && errno == EINTR);
 	assert (err == 0);
 }
-
-void process1_activation() {
-	printf("process1 \n");
-};
-
-/*void process2_activation() {
-	printf("process2\n");
-}*/
-
-void set_task_period(struct timespec * task_period) {
+void set_task_period(struct timespec * task_period, long periodSec) {
 	task_period->tv_nsec = 0;
-	task_period->tv_sec = 1;
+	task_period->tv_sec = periodSec;
 }
 
 void timespec_add(struct timespec * next_activation, struct timespec * task_period) {
 	next_activation->tv_sec += task_period->tv_sec;
 	next_activation->tv_nsec += task_period->tv_nsec;
 }
+void *task1(void *arg) {
+	pthread_barrier_wait(&barrier);
+	struct timespec next_activation, task_period;
+	set_task_period(&task_period, 1);
+	int err = clock_gettime(CLOCK_MONOTONIC, &next_activation);
+	
+	assert(err == 0);
+
+	while (1) {
+		sleep_until_next_activation(&next_activation);
+        
+		clock_t begin = clock();
+		printf("process1 \n");
+		clock_t end = clock();
+		unsigned long resp_time = (end - begin);
+		printf("Response time: %ld (us) \n", resp_time);
+		timespec_add(&next_activation, &task_period);
+
+		
+		//pthread_exit(NULL);
+	}
+};
+
+void *task2(void *arg) {
+	pthread_barrier_wait(&barrier);
+	struct timespec next_activation, task_period;
+	set_task_period(&task_period, 2);
+	int err = clock_gettime(CLOCK_MONOTONIC, &next_activation);
+	
+	assert(err == 0);
+
+	while (1) {
+		sleep_until_next_activation(&next_activation);
+        
+		clock_t begin = clock();
+		printf("process2 \n");
+		clock_t end = clock();
+		unsigned long resp_time = (end - begin);
+		printf("Response time: %ld (us) \n", resp_time);
+		timespec_add(&next_activation, &task_period);
+		//pthread_exit(NULL);
+	}
+};
 
 int main (void) {
-	struct timespec next_activation, task_period;
-	set_task_period(&task_period);
-	int err= clock_gettime(CLOCK_MONOTONIC, &next_activation);
-	assert(err == 0);
-	next_activation.tv_sec = 0;
+	
+    if (pthread_barrier_init(&barrier, NULL, 2) != 0) {
+        perror("Erreur d'initialisation de la barrière");
+        return EXIT_FAILURE;
+    }
+	pthread_t tid1;
+	if (pthread_create (&tid1, NULL, task1, NULL) != 0){
+			printf("ne marche pas\n");
+			return -1;
+	};
+	pthread_t tid2;
+	if (pthread_create (&tid2, NULL, task2, NULL) != 0){
+			printf("ne marche pas\n");
+			return -1;
+	};
 	while (1) {
-	process1_activation();
-		sleep_until_next_activation(&next_activation);
-		
-		process1_activation();
-		clock_t end = clock(); //tick par secondes, (CLOCKS_PER_SEC = 1000 000 de tick/s
-		clock_t time = ((clock_t) next_activation.tv_sec);
-		printf("%ld \n", time);
-		timespec_add(&next_activation, &task_period);
+
 	}
 }
-
 
