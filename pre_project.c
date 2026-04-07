3c38ac468ff55449fdb31d0410887d2b8e25f7d7
THEW00LY <amine.hajji1357@gmail.com>
2026-04-07 19:28:57 +0000

    Question 4 pre projet en cours, (mutex/section critique à implémenter)

--- pre_project.c
+++ pre_project.c
@@ -65,7 +65,7 @@ void *task1(void *arg) {
 
 		/* simulated workload - adjust to make scheduler effects visible */
 		volatile unsigned long dummy = 0;
-		for (unsigned long i = 0; i < load * 1000ul; ++i) {
+		for (unsigned long i = 0; i < load * 10000ul; ++i) {
 			dummy += i;
 		}
 		(void)dummy;
@@ -103,7 +103,7 @@ void *task2(void *arg) {
 
 		/* simulated workload - can be adjusted */
 		volatile unsigned long dummy = 0;
-		for (unsigned long i = 0; i < load * 500ul; ++i) {
+		for (unsigned long i = 0; i < load * 10000ul; ++i) {
 			dummy += i;
 		}
 		(void)dummy;
@@ -122,6 +122,44 @@ void *task2(void *arg) {
 	}
 };
 
+void *task3(void *arg) {
+	pthread_barrier_wait(&barrier);
+	struct timespec next_activation, task_period;
+	set_task_period(&task_period, 2);
+	int err = clock_gettime(CLOCK_MONOTONIC, &next_activation);
+	
+	assert(err == 0);
+
+	while (1) {
+		sleep_until_next_activation(&next_activation);
+
+		struct timespec t_start, t_end;
+		if (clock_gettime(CLOCK_MONOTONIC, &t_start) != 0) {
+			perror("clock_gettime");
+		}
+
+		/* simulated workload - can be adjusted */
+		volatile unsigned long dummy = 0;
+		for (unsigned long i = 0; i < load * 10000ul; ++i) {
+			dummy += i;
+		}
+		(void)dummy;
+
+		printf("process3\n");
+
+		if (clock_gettime(CLOCK_MONOTONIC, &t_end) != 0) {
+			perror("clock_gettime");
+		}
+
+		long long resp_us = (t_end.tv_sec - t_start.tv_sec) * 1000000LL +
+			(t_end.tv_nsec - t_start.tv_nsec) / 1000LL;
+		printf("Response time task 2: %lld (us)\n", resp_us);
+		timespec_add(&next_activation, &task_period);
+		//pthread_exit(NULL);
+	}
+};
+//----------------------------------------------------------------------------------------------------------
+//question 2)
 /* int main (void) {
 	pthread_attr_t attr;
     if (pthread_barrier_init(&barrier, NULL, 2) != 0) {
@@ -142,9 +180,97 @@ void *task2(void *arg) {
 
 	}
 } */
-//question 2) utiliser la commande: taskset -a 0x00000001 ./pre_project
+//----------------------------------------------------------------------------------------------------------
+//question 3) exec with: sudo taskset -c 0 ./pre_project
+//----------------------------------------------------------------------------------------------------------
+//question 4) Here we have a stable exec time for task 2 if its priority is < of task 1's
+//prio bc it can't be preempted in this case, but if they have the same prio, task 2's
+//exec time starting being non deterministic
 
-//question 3)
+/* int main (void) {
+	//attr init
+	pthread_attr_t attr;
+	int ret = pthread_attr_init(&attr);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_attr_init: %s\n", strerror(ret));
+		return EXIT_FAILURE;
+	}
+
+	ret = pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_attr_setinheritsched: %s\n", strerror(ret));
+	}
+
+	ret = pthread_attr_setschedpolicy(&attr, SCHED_RR);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_attr_setschedpolicy: %s\n", strerror(ret));
+	}
+
+	//get valid priority range for the chosen policy
+	int prio_max = sched_get_priority_max(SCHED_RR);
+	int prio_min = sched_get_priority_min(SCHED_RR);
+	if (prio_max == -1 || prio_min == -1) {
+		perror("sched_get_priority_max/min");
+	}
+
+	//prepare parameters - default: make task1 higher than task2 if possible
+	struct sched_param parameter;
+	int p_high = prio_max;
+	int p_low = (prio_max > prio_min) ? (prio_max - 1) : prio_min;
+
+	if (pthread_barrier_init(&barrier, NULL, 2) != 0) {
+		perror("Erreur d'initialisation de la barrière");
+		return EXIT_FAILURE;
+	}
+
+	pthread_t tid1, tid2;
+
+	//create task1 with higher priority
+	parameter.sched_priority = p_high;
+	ret = pthread_attr_setschedparam(&attr, &parameter);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_attr_setschedparam(task1): %s\n", strerror(ret));
+	}
+
+	ret = pthread_create(&tid1, &attr, task1, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_create tid1: %s\n", strerror(ret));
+		return -1;
+	}
+
+	//print actual scheduling of tid1
+	int pol; struct sched_param actual;
+	if (pthread_getschedparam(tid1, &pol, &actual) == 0) {
+		printf("tid1 policy=%d priority=%d\n", pol, actual.sched_priority);
+	}
+
+	//create task2 with lower priority
+	parameter.sched_priority = p_high;
+	ret = pthread_attr_setschedparam(&attr, &parameter);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_attr_setschedparam(task2): %s\n", strerror(ret));
+	}
+
+	ret = pthread_create(&tid2, &attr, task2, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_create tid2: %s\n", strerror(ret));
+		return -1;
+	}
+
+	if (pthread_getschedparam(tid2, &pol, &actual) == 0) {
+		printf("tid2 policy=%d priority=%d\n", pol, actual.sched_priority);
+	}
+
+	//cleanup attributes and wait for threads
+	pthread_attr_destroy(&attr);
+
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
+
+	pthread_barrier_destroy(&barrier);
+	return 0;
+} */
+//----------------------------------------------------------------------------------------------------------
 
 int main (void) {
 	/* attr init */
@@ -175,14 +301,15 @@ int main (void) {
 	/* prepare parameters - default: make task1 higher than task2 if possible */
 	struct sched_param parameter;
 	int p_high = prio_max;
-	int p_low = (prio_max > prio_min) ? (prio_max - 1) : prio_min;
+	int p_mid = (prio_max > prio_min) ? (prio_max - 1) : prio_min;
+	int p_low = (p_mid > prio_min) ? (p_mid - 1) : prio_min;
 
 	if (pthread_barrier_init(&barrier, NULL, 2) != 0) {
 		perror("Erreur d'initialisation de la barrière");
 		return EXIT_FAILURE;
 	}
 
-	pthread_t tid1, tid2;
+	pthread_t tid1, tid2, tid3;
 
 	/* create task1 with higher priority */
 	parameter.sched_priority = p_high;
@@ -220,11 +347,29 @@ int main (void) {
 		printf("tid2 policy=%d priority=%d\n", pol, actual.sched_priority);
 	}
 
+	/* create task3 with mid priority */
+	parameter.sched_priority = p_mid;
+	ret = pthread_attr_setschedparam(&attr, &parameter);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_attr_setschedparam(task2): %s\n", strerror(ret));
+	}
+
+	ret = pthread_create(&tid3, &attr, task2, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_create tid2: %s\n", strerror(ret));
+		return -1;
+	}
+
+	if (pthread_getschedparam(tid3, &pol, &actual) == 0) {
+		printf("tid2 policy=%d priority=%d\n", pol, actual.sched_priority);
+	}
+
 	/* cleanup attributes and wait for threads */
 	pthread_attr_destroy(&attr);
 
 	pthread_join(tid1, NULL);
 	pthread_join(tid2, NULL);
+	pthread_join(tid3, NULL);
 
 	pthread_barrier_destroy(&barrier);
 	return 0;
